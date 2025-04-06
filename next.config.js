fdd9552cd085ba8e75d64c20c10c05127065c54c
Glenn Isaj <glennisaj24@gmail.com>
2025-04-05 19:21:08 -0500

    rename next config to js extension

---
+++ next.config.js
@@ -0,0 +1,20 @@
+/** @type {import('next').NextConfig} */
+module.exports = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  async headers() {
+    return [
+      {
+        source: '/api/:path*',
+        headers: [
+          { key: 'Access-Control-Allow-Origin', value: 'your-domain.com' },
+          { key: 'Access-Control-Allow-Methods', value: 'GET,POST' }
+        ]
+      }
+    ]
+  }
+} 
